7d3826d3a6937a4c3c4749d976a358e17649ec89
germanny <germanny@gmail.com>
2020-11-15 16:14:39 -0600

    update port for heroku

--- app.js
+++ app.js
@@ -32,6 +32,10 @@ app.use((err, req, res, next) => {
 });
 
 // turn on the app
-app.listen(3000, () => {
-  console.log('We\'re running on localhost:3000!')
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  if (port === 3000) {
+    console.log('We\'re running on localhost:3000!')
+  }
 });
